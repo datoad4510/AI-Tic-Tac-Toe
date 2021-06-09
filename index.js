43fac3c78ed79011d552f9354f0d3b4273a12d1c
user1 <54779055+datoad4510@users.noreply.github.com>
2021-06-09 22:58:27 +0400

    debugging

--- index.js
+++ index.js
@@ -48,11 +48,23 @@ app.listen(port, () => {
 
 /* ! Code for running CPP process below */
 
-const spawn = require("child_process").spawn;
+// const spawn = require("child_process").spawn;
 
-console.log(process.env.PATH);
+// console.log(process.env.PATH);
 
-const CppApp = spawn("tic_tac_toe.out");
+// const CppApp = spawn("tic_tac_toe.out");
+
+(function () {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log("spawn called");
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
 
 /*
 function getChildScores() {
